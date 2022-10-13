1624d39009a0db6041681e8ba4f3b7fc61e7058f
Matheus Limão Brites <majortheus@gmail.com>
2022-10-13 01:08:25 -0300

    finished course, waiting live event

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -3,12 +3,13 @@ import { Check } from "phosphor-react";
 
 import { clsx } from "clsx";
 
-export type CheckboxProps = {};
+export type CheckboxProps = {} & CheckboxPremitive.CheckboxProps;
 
-export function Checkbox({}: CheckboxProps) {
+export function Checkbox({ ...rest }: CheckboxProps) {
   return (
     <CheckboxPremitive.Root
       className={clsx("w-6 h-6 p-[2px] bg-gray-800 rounded", {})}
+      {...rest}
     >
       <CheckboxPremitive.Indicator asChild>
         <Check weight="bold" className="h-5 w-5 text-cyan-500" />
